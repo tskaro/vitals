2d6e6fc4c054cf838e8e9137345665ce74e3f97b
tskaro <tskaro@gmail.com>
2021-02-23 22:12:34 +0400

    ready for heroku

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_restful import Api
 from resources.vitals import from_db_to_api
 from resources.users import User_registration
@@ -7,11 +7,21 @@ from security import authentication, identity
 
 app = Flask(__name__)
 app.secret_key = "unbelievable_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Health_data.db'
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Health_data.db'
 
 api = Api(app)
 jwt = JWT(app, authentication, identity)
+
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return redirect("https://github.com/tskaro/vitals")
 
 
 api.add_resource(from_db_to_api, '/vitals/<int:patient_id>')
@@ -19,5 +29,6 @@ api.add_resource(User_registration, '/registration')
 
 if __name__ == '__main__':
     from db import db
+
     db.init_app(app)
     app.run(debug=True, port=500)
